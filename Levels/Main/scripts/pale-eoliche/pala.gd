10df3d92df6f1b1cca23c2048a9a42869f1830e0
Farfa7886 <50551069+Farfa7886@users.noreply.github.com>
2023-06-09 15:31:19 +0200

    Inital commit
    game files

---
+++ Levels/Main/scripts/pale-eoliche/pala.gd
@@ -0,0 +1,17 @@
+extends MeshInstance
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _process(delta: float):
+	rotate_z(0.01)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
